68943496644dcaf9aee25fb98895199c1365fcd5
VJKamat <vijaykamat@live.com>
2021-05-14 19:43:20 +0530

    Updates

---
+++ code.js
@@ -0,0 +1,20 @@
+$(function () {
+    $('.button-close').click(function () {
+        $('.overlayCamera').addClass('is-open');
+        return false;
+    });
+
+    $('.close-btn').click(function () {
+        $('.overlayCamera').removeClass('is-open');
+    });
+
+
+    $('.btn-Awards').click(function () {
+        $('.overlayAwards').addClass('is-open');
+        return false;
+    });
+
+    $('.close-btn').click(function () {
+        $('.overlayAwards').removeClass('is-open');
+    });
+});
