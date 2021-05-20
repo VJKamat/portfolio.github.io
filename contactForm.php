9ce80fd29a63d112881d339a95f8b3f53402c82b
VJKamat <vijaykamat@live.com>
2021-05-20 23:42:07 +0530

    update

--- contactForm.php
+++ contactForm.php
@@ -1,25 +1,5 @@
 <?php
 
-if(isset($_POST['email']) && $_POST['email'] != '')
-{
-
-    $userName = $_POST['name'];
-    $userEmail = $_POST['email'];
-    $subject = "Message from Porfolio";
-    $message = $_POST['message'];
-
-
-    $to = "vijaykamat@live.com";
-    $body = "";
-
-
-    $body.= "From ".$userName. "\r\n";
-    $body.= "Email ".$userEmail. "\r\n";
-    $body.= "Message ".$message. "\r\n";
-
-    
-    mail($to, $subject, $body);
-    header("Location: index.html")
-}
+echo "hello";
 
 ?>
