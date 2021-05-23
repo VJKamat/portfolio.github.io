5c853fa187ea957d01af3a93e0d1ed3707de8dc7
VJKamat <vijaykamat@live.com>
2021-05-23 18:14:41 +0530

    Updates

--- contactForm.php
+++ contactForm.php
@@ -1,10 +1,9 @@
 <?php
-
-
-
-echo"Hello";
-
-
+$toEmail = "vijaykaamat@live.com";
+$mailHeaders = "From: " . $_POST["name"] . "<". $_POST["email"] .">\r\n";
+if(mail($toEmail, $_POST["subject"], $_POST["message"], $mailHeaders)) {
+print "<p class='success'>Messege Sent. Thank you.</p>";
+} else {
+print "<p class='Error'>Looks like something went wrong.</p>";
+}
 ?>
-
-
